5a4d706bfc05664fb13e151f5b0af00c2914fca2
Jakub Malý <xmaly@fi.muni.cz>
2023-10-01 17:14:29 +0200

    change the app name

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import "../styles/TopBar.css";
 
 export default function TopBar() {
-  return <div className="top-bar">CV Generator</div>;
+  return <div className="top-bar">CV Builder</div>;
 }
