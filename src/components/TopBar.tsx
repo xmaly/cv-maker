98187fdcd3c388b9f42cfbb4a76d0cbe42ad1263
Jakub Malý <xmaly@fi.muni.cz>
2023-10-02 13:03:53 +0200

    add author + link to website

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -2,5 +2,19 @@ import React from "react";
 import "../styles/TopBar.css";
 
 export default function TopBar() {
-  return <div className="top-bar">CV Builder</div>;
+  return (
+    <div className="top-bar">
+      <div className="name">CV Builder</div>
+      <div className="caption grey">
+        <div className="author">
+          <span className="grey">by</span> Jakub Malý
+        </div>
+        <a href="https://xmaly.github.io/resume/" target="_blank">
+          <span className="material-symbols-outlined website">
+            captive_portal
+          </span>
+        </a>
+      </div>
+    </div>
+  );
 }
